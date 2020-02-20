202b26cae40820767e7d29c7d87fe26c74fea623
Pere Conte <pere.conte1@gmail.com>
2020-02-20 19:12:18 +0100

    millors classes

--- solution.py
+++ solution.py
@@ -1,12 +1,13 @@
 class Book:
-    def __init__(self, id, score):
+    def __init__(self, id, score, cont):
         self.id = id
         self.score = score
+        self.cont = cont # vector<Lib>: són les llibreries que el contenen
 
 class Lib:
-    def __init__(self, books, size, signup, ship):
-        self.books = books # vector<Book>
-        self.size = size
+    def __init__(self, id, signup, ship, available):
+        self.id = id
         self.signup = signup
         self.ship = ship
+        self.available = available
 
